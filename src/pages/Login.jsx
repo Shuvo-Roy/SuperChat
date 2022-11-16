1a4689dadce9d3c1baae1f7b038eb7db47ecbdd1
Shuvo Roy <shuvoroysp@gmail.com>
2022-11-16 21:00:30 +0600

    Login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,1 +1,20 @@
 
+import React from "react";
+import Add from"../images/addAvatar.png";
+export const Login = () => {
+  return (
+    <div className="formContainer">
+      <div className="formWrapper">
+        <span className="logo">SuperChat</span>
+        <span className="title">Register</span>
+        <form>
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="password" />
+          <button>Sign In</button>
+        </form>
+        <p>You don't have an account? Register</p>
+      </div>
+    </div>
+  );
+};
+export default Login;
