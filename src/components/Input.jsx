2fd6c1b1a2f0f7f0e8ea1b49a686212d9f59ad13
Shuvo Roy <shuvoroysp@gmail.com>
2022-11-18 19:49:34 +0600

    basic design

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
-
+import Img from "../images/img.png"
+import Attach from "../images/attach.png"
 const Input = () => {
   return (
-    <div>Input</div>
+    <div className='input'>
+      
+      <div className="send">
+        <img src={Attach} alt=""/>
+        <input type="file" style={{display:"none"}} id="file"></input>
+        <lable htmlFor="file">
+          <img src={Img} alt=""/>
+        </lable>
+      </div>
+      <input type="text" placeholder='Write Massage'/>
+      <button> Send</button>
+    </div>
   )
 }
 export  default Input
--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
-
+import IMG from "../images/user.jpg"
 const Message = () => {
   return (
-    <div>Message</div>
+    <div className='message owner'>
+      <div className='messageInfo'>
+        <img src={IMG} alt=""/>
+        <span>just now</span>
+      </div>
+      <div className='messageContent'>
+        <p>Hello</p>
+        <img src={IMG} alt=""/>
+      </div>
+    </div>
   )
 }
 export  default Message
