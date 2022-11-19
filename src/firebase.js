b5e2b0068443f421ec7a835ec14bf792d2882fb2
Shuvo Roy <shuvoroysp@gmail.com>
2022-11-19 22:13:08 +0600

    adding firebase

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDfHa6szmqhO7cCVyZHPBaZTKvQDvEbfrw",
+  authDomain: "superchat-shuvo.firebaseapp.com",
+  projectId: "superchat-shuvo",
+  storageBucket: "superchat-shuvo.appspot.com",
+  messagingSenderId: "300029643371",
+  appId: "1:300029643371:web:fc7c0585f506a8ea373f2e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
