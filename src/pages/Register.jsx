0d94dceee596b207cc0850622f1e5e02bfaa3646
Shuvo Roy <shuvoroysp@gmail.com>
2022-11-21 10:21:27 +0600

    some user authentication

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,12 +1,50 @@
-import React from "react";
+import React ,{useState} from "react";
 import Add from"../images/addAvatar.png";
+import {createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import {auth ,storage} from "../firebase";
+import { async } from "@firebase/util";
+import {ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+
+
+
+
 export const Register = () => {
+  const[err, setErr] = useState(false);
+  const handleSubmit= async(e)=>{
+    e.preventDefault()
+    const displayName = e.target[0].value;
+    const email = e.target[1].value;
+    const password = e.target[2].value;
+    const file = e.target[3].files[0];
+
+    try{
+      const res = await createUserWithEmailAndPassword(auth, email, password);
+      const storageRef = ref(storage, displayName);
+
+      const uploadTask = uploadBytesResumable(storageRef, file);
+      uploadTask.on(
+        (error) => {
+          setErr(true);
+        }, 
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then(async(downloadURL) => {
+            await updateProfile(res.user,{
+              displayName,
+              photoURL: downloadURL,
+            });
+          });
+        }
+      );
+    }catch{
+      setErr(true)
+    }
+  }
   return (
     <div className="formContainer">
       <div className="formWrapper">
         <span className="logo">SuperChat</span>
         <span className="title">Register</span>
-        <form>
+        <form on onSubmit={handleSubmit}>
           <input type="text" placeholder="display name" />
           <input type="email" placeholder="Email" />
           <input type="password" placeholder="password" />
@@ -15,6 +53,7 @@ export const Register = () => {
             <img src={Add} alt=""/><span>Add an Avatar</span>
           </label>
           <button>Sign Up</button>
+          {err && <span>Something went wrong</span>}
         </form>
         <p>You do have an account? Login</p>
       </div>
